1111f4a1ac1660b66313c15d8610437665a3ee1c
Your Name <you@example.com>
2021-02-11 16:58:42 -0500

    We have implemented some events and validations of our controller

--- src/model/Classroom.java
+++ src/model/Classroom.java
@@ -12,12 +12,37 @@ public class Classroom {
         accounts = new ArrayList<UserAccount>();
     }
 
-    public void addAccount(String userName, String password, Image userIcon, Gender gender, Career career,
+    public void addAccount(String userName, String password, Image userIcon, String gender, String career,
             String birthday, String browser) {
-        accounts.add(new UserAccount(userName, password, userIcon, gender, career, birthday, browser));
+        Gender genderEnum = null;
+        Career careerEnum = null;
+        switch (gender) {
+            case "male":
+                genderEnum = Gender.MALE;
+                break;
+            case "female":
+                genderEnum = Gender.FEMALE;
+                break;
+            case "other":
+                genderEnum = Gender.OTHER;
+                break;
+        }
+        switch (gender) {
+            case "Software Engineering":
+                careerEnum = Career.SOFTWARE_ENGINEERING;
+                break;
+            case "Telematic Engineering":
+                careerEnum = Career.SYSTEM_ENGINEERING;
+                break;
+            case "System Engineering":
+                careerEnum = Career.TELEMATIC_ENGINEERING;
+                break;
+        }
+        accounts.add(new UserAccount(userName, password, userIcon, genderEnum, careerEnum, birthday, browser));
     }
 
     public List<UserAccount> getAccounts() {
         return accounts;
     }
+
 }
--- src/ui/ClassroomGUI.java
+++ src/ui/ClassroomGUI.java
@@ -3,6 +3,7 @@ package ui;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
@@ -11,13 +12,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.*;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.*;
 import javafx.scene.layout.*;
 import javafx.stage.FileChooser;
-import model.Classroom;
+import model.*;
 
 public class ClassroomGUI implements Initializable {
     private Classroom classroom;
+
+    private Image icon;
 
     public ClassroomGUI(Classroom cr) {
         this.classroom = cr;
@@ -40,6 +44,27 @@ public class ClassroomGUI implements Initializable {
 
     @FXML
     private ToggleGroup gender;
+
+    @FXML
+    private RadioButton maleRb;
+
+    @FXML
+    private RadioButton femaleRb;
+
+    @FXML
+    private RadioButton otherRb;
+
+    @FXML
+    private CheckBox cbSoft;
+
+    @FXML
+    private CheckBox cbSys;
+
+    @FXML
+    private CheckBox cbTel;
+
+    @FXML
+    private DatePicker dateBirth;
 
     @FXML
     private TextField txtUserName;
@@ -91,10 +116,23 @@ public class ClassroomGUI implements Initializable {
 
     @FXML
     void singIn(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("accountList.fxml"));
-        fxmlLoader.setController(this);
-        Parent dash = fxmlLoader.load();
-        mainPane.getChildren().setAll(dash);
+        for (int i = 0; i < classroom.getAccounts().size(); i++) {
+            UserAccount accounts = classroom.getAccounts().get(i);
+            if (txtUserName.getText().equals(accounts.getUserName())
+                    && txtPassword.getText().equals(accounts.getPassword())) {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("accountList.fxml"));
+                fxmlLoader.setController(this);
+                Parent dash = fxmlLoader.load();
+                mainPane.getChildren().setAll(dash);
+            } else {
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setHeaderText("Pareme bolas");
+                alert.setTitle("Error");
+                alert.setContentText("Usario o cantra fail");
+                alert.showAndWait();
+            }
+        }
+
     }
 
     @FXML
@@ -107,9 +145,9 @@ public class ClassroomGUI implements Initializable {
     }
 
     public void getComboItems() {
-        ObservableList<String> browserList = FXCollections.observableArrayList("Chrome", "Mozilla", "Safari", "Opera",
-                "Other");
-        cbBrowsers.setValue("Chrome");
+        ObservableList<String> browserList = FXCollections.observableArrayList("", "Chrome", "Mozilla", "Safari",
+                "Opera", "Other");
+        cbBrowsers.setValue("");
         cbBrowsers.setItems(browserList);
     }
 
@@ -122,31 +160,90 @@ public class ClassroomGUI implements Initializable {
     }
 
     @FXML
-    void birthdayDate(ActionEvent event) {
-
-    }
-
-    @FXML
-    void browsers(ActionEvent event) {
-
-    }
-
-    @FXML
-    void createAccount(ActionEvent event) {
-
+    public void createAccount(ActionEvent event) {
+        if (userUp.getText().equals("") || pswUp.getText().equals("")
+                || urlBrowse.getText().equals("Path does not exist") || gender.getSelectedToggle() == null
+                || dateBirth.getValue() == null || cbBrowsers.getValue().equals("") || urlBrowse.getText().equals("")) {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setHeaderText("Pareme bolas");
+            alert.setTitle("Warning");
+            alert.setContentText("Hay campos vacíos o imagen no cargada o inclusive un genero no seleccionado");
+            alert.setHeight(400);
+            alert.showAndWait();
+        } else if (pswUp.getText().length() <= 6) {
+            Alert alert1 = new Alert(AlertType.ERROR);
+            alert1.setHeaderText("Pareme bolas");
+            alert1.setTitle("Error");
+            alert1.setContentText("Contraseña muy debil");
+            alert1.showAndWait();
+        } else if (!cbSoft.isSelected() & !cbSys.isSelected() & !cbTel.isSelected()) {
+            Alert alert1 = new Alert(AlertType.INFORMATION);
+            alert1.setHeaderText("No has estudiado");
+            alert1.setTitle("Information");
+            alert1.setContentText("Para crear una cuenta debe tener al menos una de estas profesiones");
+            alert1.setHeight(400);
+            alert1.showAndWait();
+        } else {
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Confirmation Message");
+            alert.setHeaderText("Look, a Confirmation Dialog");
+            alert.setContentText("Are you ok with this?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK) {
+                Alert alert2 = new Alert(AlertType.INFORMATION);
+                alert2.setTitle("redux and react");
+                alert2.setHeaderText("The User " + userUp.getText() + " have been added sucesfully");
+                alert2.setContentText("Take it easy bro!");
+                alert2.showAndWait();
+                classroom.addAccount(userUp.getText(), pswUp.getText(), icon, gender.getSelectedToggle().toString(),
+                        getCareers(), dateBirth.getValue().toString(), cbBrowsers.getValue());
+            }
+            userUp.setText("");
+            pswUp.setText("");
+            urlBrowse.setText("");
+            icon = null;
+            maleRb.setSelected(false);
+            femaleRb.setSelected(false);
+            otherRb.setSelected(false);
+            cbSoft.setSelected(false);
+            cbSys.setSelected(false);
+            cbTel.setSelected(false);
+            dateBirth.setValue(null);
+            cbBrowsers.setValue("");
+        }
     }
 
     @FXML
     public void fileChooser(ActionEvent event) {
+        Image userIcon = null;
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("PNG", "*.png"));
         File selectedFile = fileChooser.showOpenDialog(null);
         if (selectedFile != null) {
             urlBrowse.setText(selectedFile.getPath());
+            userIcon = new Image(selectedFile.toURI().toString());
+            setUserIcon(userIcon);
         } else {
             urlBrowse.setText("Path does not exist");
         }
+    }
 
+    public void setUserIcon(Image userIcon) {
+        this.icon = userIcon;
+    }
+
+    public String getCareers() {
+        String careers = "";
+        if (cbSoft.isSelected()) {
+            careers += cbSoft.getText();
+        } else if (cbSys.isSelected()) {
+            careers += cbSys.getText();
+        } else if (cbTel.isSelected()) {
+            careers += cbTel.getText();
+        }
+        return careers;
     }
 
     @FXML
@@ -176,6 +273,16 @@ public class ClassroomGUI implements Initializable {
 
     @FXML
     void telEgn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void birthdayDate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void browsers(ActionEvent event) {
 
     }
 
